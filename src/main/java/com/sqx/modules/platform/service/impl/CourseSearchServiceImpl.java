2505abeca8064602c63869960c74a2702f22c89d
周勃辰 <Kashiwagi_Eri@outlook.com>
2024-11-21 15:43:53 +0800

    feat(platform): 为短剧搜索和剧单页面添加缓存支持
    - 在 CourseSearchServiceImpl 和 SeriesServiceImpl 中添加了缓存逻辑
    - 使用 RedisUtils 进行缓存存储和获取
    - 优化了数据库查询性能，减少了重复查询
    - 新增了 flushSeries 方法来清除剧单缓存

--- src/main/java/com/sqx/modules/platform/service/impl/CourseSearchServiceImpl.java
+++ src/main/java/com/sqx/modules/platform/service/impl/CourseSearchServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sqx.common.utils.RedisUtils;
 import com.sqx.common.utils.Result;
 import com.sqx.modules.course.dao.CourseDao;
 import com.sqx.modules.course.entity.Course;
@@ -15,6 +16,7 @@ import com.sqx.modules.platform.entity.Series;
 import com.sqx.modules.platform.entity.SeriesCourse;
 import com.sqx.modules.platform.service.CoursePerformerService;
 import com.sqx.modules.platform.service.CourseSearchService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -39,24 +41,33 @@ public class CourseSearchServiceImpl extends ServiceImpl<CourseDao, Course> impl
     private PerformerService performerService;
     @Resource
     private CoursePerformerService coursePerformerService;
+    @Resource
+    private RedisUtils redisUtils;
 
     @Override
     public Result searchCourse(Page<Course> page, String keyword, Long areaId, Long classifyId, Long platformId, Long isPrice, String sortField, Integer sort) {
-
-        Page<Course> coursePage = courseDao.searchCourse(page, keyword, areaId, classifyId, platformId, isPrice, sortField, sort);
-        if (coursePage != null && coursePage.getRecords() != null) {
-            for (Course course : coursePage.getRecords()) {
-                // 查询演员
-                List<CoursePerformer> coursePerformerList = coursePerformerService.list(new QueryWrapper<CoursePerformer>().eq("course_id", course.getCourseId()));
-                if (!coursePerformerList.isEmpty()) {
-                    List<Performer> performerList = performerService.list(new QueryWrapper<Performer>().in("id", coursePerformerList.stream().map(CoursePerformer::getPerformerId).toArray()));
-                    course.setPerformerList(performerList);
+        Page<Course> coursePage;
+        // 检查缓存
+        String searchCourseName = "page_searchCourse_" + page.getCurrent() + "_" + page.getSize() + "_" + keyword + "_" + areaId + "_" + classifyId + "_" + platformId + "_" + isPrice + "_" + sortField + "_" + sort;
+        String cache = redisUtils.get(searchCourseName);
+        // 如果缓存中没有数据，则从数据库中查询
+        if (StringUtils.isEmpty(cache)) {
+            coursePage = courseDao.searchCourse(page, keyword, areaId, classifyId, platformId, isPrice, sortField, sort);
+            if (coursePage != null && coursePage.getRecords() != null) {
+                for (Course course : coursePage.getRecords()) {
+                    // 查询演员
+                    List<CoursePerformer> coursePerformerList = coursePerformerService.list(new QueryWrapper<CoursePerformer>().eq("course_id", course.getCourseId()));
+                    if (!coursePerformerList.isEmpty()) {
+                        List<Performer> performerList = performerService.list(new QueryWrapper<Performer>().in("id", coursePerformerList.stream().map(CoursePerformer::getPerformerId).toArray()));
+                        course.setPerformerList(performerList);
+                    }
                 }
             }
+            redisUtils.set(searchCourseName, coursePage);
+        } else {
+            coursePage = redisUtils.get(searchCourseName, Page.class);
         }
-
         return Result.success().put("data", coursePage);
     }
-
 
 }
--- src/main/java/com/sqx/modules/platform/service/impl/SeriesServiceImpl.java
+++ src/main/java/com/sqx/modules/platform/service/impl/SeriesServiceImpl.java
@@ -3,6 +3,7 @@ package com.sqx.modules.platform.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sqx.common.utils.RedisUtils;
 import com.sqx.common.utils.Result;
 import com.sqx.modules.course.dao.CourseCollectDao;
 import com.sqx.modules.course.entity.Course;
@@ -20,6 +21,7 @@ import com.sqx.modules.platform.service.CoursePerformerService;
 import com.sqx.modules.platform.service.CourseScoreService;
 import com.sqx.modules.platform.service.SeriesCourseService;
 import com.sqx.modules.platform.service.SeriesService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -52,82 +54,118 @@ public class SeriesServiceImpl extends ServiceImpl<SeriesDao, Series> implements
     private PerformerService performerService;
     @Resource
     private CourseCollectService courseCollectService;
+    @Resource
+    private RedisUtils redisUtils;
 
     @Override
     public Result selectSeriesPage(Page<Series> page, Series series) {
-        Page<Series> seriesPage = this.page(page, new QueryWrapper<>(series).orderByAsc("sort"));
-        if (seriesPage != null && seriesPage.getRecords() != null) {
-            for (Series series1 : seriesPage.getRecords()) {
-                List<SeriesCourse> seriesCourseList = seriesCourseService.list(new QueryWrapper<SeriesCourse>().eq("series_id", series1.getSeriesId()));
-                if (!seriesCourseList.isEmpty()) {
-                    List<Long> courseIdList = seriesCourseList.stream().map(SeriesCourse::getCourseId).collect(Collectors.toList());
-                    List<Course> courseList = courseService.list(new QueryWrapper<Course>().in("course_id", courseIdList));
-                    series1.setCourseIds(courseIdList);
-                    series1.setCourseList(courseList);
-
+        Page<Series> seriesPage;
+        // 检查缓存
+        String seriesPageName = "page_series_" + page.getCurrent() + "_" + page.getSize();
+        String cache = redisUtils.get(seriesPageName);
+        // 如果缓存中没有数据，则从数据库中查询
+        if (StringUtils.isEmpty(cache)) {
+            seriesPage = this.page(page, new QueryWrapper<>(series).orderByAsc("sort"));
+            if (seriesPage != null && seriesPage.getRecords() != null) {
+                for (Series series1 : seriesPage.getRecords()) {
+                    List<SeriesCourse> seriesCourseList = seriesCourseService.list(new QueryWrapper<SeriesCourse>().eq("series_id", series1.getSeriesId()));
+                    if (!seriesCourseList.isEmpty()) {
+                        List<Long> courseIdList = seriesCourseList.stream().map(SeriesCourse::getCourseId).collect(Collectors.toList());
+                        List<Course> courseList = courseService.list(new QueryWrapper<Course>().in("course_id", courseIdList));
+                        series1.setCourseIds(courseIdList);
+                        series1.setCourseList(courseList);
+                    }
                 }
             }
+            // 缓存数据
+            redisUtils.set(seriesPageName, seriesPage);
+        } else {
+            // 从缓存中获取数据
+            seriesPage = redisUtils.get(seriesPageName, Page.class);
         }
         return Result.success().put("data", seriesPage);
     }
 
     @Override
     public Result selectSeriesById(Serializable id) {
-        Series series = this.getById(id);
-        if (series != null) {
-            List<SeriesCourse> seriesCourseList = seriesCourseService.list(new QueryWrapper<SeriesCourse>().eq("series_id", id));
-            if (!seriesCourseList.isEmpty()) {
-                List<Long> courseIdList = seriesCourseList.stream().map(SeriesCourse::getCourseId).collect(Collectors.toList());
-                // List<Course> courseList = courseService.list(new QueryWrapper<Course>().in("course_id", courseIdList));
-                series.setCourseIds(courseIdList);
-                // series.setCourseList(courseList);
+        Series series;
+        // 检查缓存
+        String seriesName = "seriesId_" + id;
+        String cache = redisUtils.get(seriesName);
+        // 如果缓存中没有数据，则从数据库中查询
+        if (StringUtils.isEmpty(cache)) {
+            series = this.getById(id);
+            if (series != null) {
+                List<SeriesCourse> seriesCourseList = seriesCourseService.list(new QueryWrapper<SeriesCourse>().eq("series_id", id));
+                if (!seriesCourseList.isEmpty()) {
+                    List<Long> courseIdList = seriesCourseList.stream().map(SeriesCourse::getCourseId).collect(Collectors.toList());
+                    // List<Course> courseList = courseService.list(new QueryWrapper<Course>().in("course_id", courseIdList));
+                    series.setCourseIds(courseIdList);
+                    // series.setCourseList(courseList);
+                }
             }
+            redisUtils.set(seriesName, series);
+        } else {
+            series = redisUtils.get(seriesName, Series.class);
         }
         return Result.success().put("data", series);
     }
 
     @Override
     public Result selectSeriesById(Long userId, Serializable id) {
-        Series series = this.getById(id);
-        if (series != null) {
-            List<SeriesCourse> seriesCourseList = seriesCourseService.list(new QueryWrapper<SeriesCourse>().eq("series_id", id));
-            if (!seriesCourseList.isEmpty()) {
-                List<Long> courseIdList = seriesCourseList.stream().map(SeriesCourse::getCourseId).collect(Collectors.toList());
-                List<Course> courseList = courseService.list(new QueryWrapper<Course>().in("course_id", courseIdList));
-                courseList.forEach(course -> {
-                    // 查询总分
-                    if (course.getScore() == null) {
-                        List<CourseScore> scores = courseScoreService.list(new QueryWrapper<CourseScore>().eq("course_id", course.getCourseId()));
-                        // 计算平均分
-                        if (!scores.isEmpty()) {
-                            course.setScore(scores.stream().map(CourseScore::getScore).reduce(BigDecimal::add).orElse(new BigDecimal(0)).divide(new BigDecimal(scores.size()), 1, RoundingMode.HALF_UP));
+        Series series;
+        // 检查缓存
+        String seriesName = "seriesId_app_" + id;
+        String cache = redisUtils.get(seriesName);
+        // 如果缓存中没有数据，则从数据库中查询
+        if (StringUtils.isEmpty(cache)) {
+            series = this.getById(id);
+            if (series != null) {
+                List<SeriesCourse> seriesCourseList = seriesCourseService.list(new QueryWrapper<SeriesCourse>().eq("series_id", id));
+                if (!seriesCourseList.isEmpty()) {
+                    List<Long> courseIdList = seriesCourseList.stream().map(SeriesCourse::getCourseId).collect(Collectors.toList());
+                    List<Course> courseList = courseService.list(new QueryWrapper<Course>().in("course_id", courseIdList));
+                    courseList.forEach(course -> {
+                        // 查询总分
+                        if (course.getScore() == null) {
+                            List<CourseScore> scores = courseScoreService.list(new QueryWrapper<CourseScore>().eq("course_id", course.getCourseId()));
+                            // 计算平均分
+                            if (!scores.isEmpty()) {
+                                course.setScore(scores.stream().map(CourseScore::getScore).reduce(BigDecimal::add).orElse(new BigDecimal(0)).divide(new BigDecimal(scores.size()), 1, RoundingMode.HALF_UP));
+                            }
                         }
-                    }
-                    // 查询演员
-                    List<CoursePerformer> coursePerformerList = coursePerformerService.list(new QueryWrapper<CoursePerformer>().eq("course_id", course.getCourseId()));
-                    if (!coursePerformerList.isEmpty()) {
-                        List<Performer> performerList = performerService.list(new QueryWrapper<Performer>().in("id", coursePerformerList.stream().map(CoursePerformer::getPerformerId).collect(Collectors.toList())));
-                        course.setPerformerList(performerList);
-                    }
-                    // 查询用户是否登录
-                    if (userId != null) {
-                        // 查询用户是否收藏了该短剧
-                        course.setIsCollect(courseCollectService.count(new QueryWrapper<CourseCollect>()
-                                .eq("user_id", userId).eq("classify", 1).eq("course_id", course.getCourseId())));
-
-                        // 查询用户是否看过了该短剧
-                        course.setIsView(courseCollectService.count(new QueryWrapper<CourseCollect>()
-                                .eq("user_id", userId).eq("classify", 3).eq("course_id", course.getCourseId())));
-
-                        // 查询用户的评分
-                        // CourseScore courseScore = courseScoreService.getOne(new QueryWrapper<CourseScore>()
-                        //         .eq("user_id", userId).eq("course_id", course.getCourseId()));
-                    }
-
-                });
-                // series.setCourseIds(courseIdList);
-                series.setCourseList(courseList);
+                        // 查询演员
+                        List<CoursePerformer> coursePerformerList = coursePerformerService.list(new QueryWrapper<CoursePerformer>().eq("course_id", course.getCourseId()));
+                        if (!coursePerformerList.isEmpty()) {
+                            List<Performer> performerList = performerService.list(new QueryWrapper<Performer>().in("id", coursePerformerList.stream().map(CoursePerformer::getPerformerId).collect(Collectors.toList())));
+                            course.setPerformerList(performerList);
+                        }
+                        // 查询用户信息提到缓存外面
+                    });
+                    // series.setCourseIds(courseIdList);
+                    series.setCourseList(courseList);
+                }
             }
+            redisUtils.set(seriesName, series);
+        } else {
+            series = redisUtils.get(seriesName, Series.class);
+        }
+
+        // 查询用户是否登录
+        if (userId != null && series != null && !series.getCourseList().isEmpty()) {
+            series.getCourseList().forEach(course -> {
+                // 查询用户是否收藏了该短剧
+                course.setIsCollect(courseCollectService.count(new QueryWrapper<CourseCollect>()
+                        .eq("user_id", userId).eq("classify", 1).eq("course_id", course.getCourseId())));
+
+                // 查询用户是否看过了该短剧
+                course.setIsView(courseCollectService.count(new QueryWrapper<CourseCollect>()
+                        .eq("user_id", userId).eq("classify", 3).eq("course_id", course.getCourseId())));
+
+                // 查询用户的评分
+                // CourseScore courseScore = courseScoreService.getOne(new QueryWrapper<CourseScore>()
+                //         .eq("user_id", userId).eq("course_id", course.getCourseId()));
+            });
         }
         return Result.success().put("data", series);
     }
@@ -136,7 +174,7 @@ public class SeriesServiceImpl extends ServiceImpl<SeriesDao, Series> implements
     @Transactional(rollbackFor = Exception.class)
     public Result insertSeries(Series series) {
         if (this.save(series)) {
-            // 修改演员关系
+            // 修改剧单关系
             if (!series.getCourseIds().isEmpty()) {
                 seriesCourseService.remove(new QueryWrapper<SeriesCourse>().eq("series_id", series.getSeriesId()));
                 for (Long courseId : series.getCourseIds()) {
@@ -144,6 +182,7 @@ public class SeriesServiceImpl extends ServiceImpl<SeriesDao, Series> implements
                     seriesCourseService.save(seriesCourse);
                 }
             }
+            this.flushSeries();
             return Result.success();
         }
         return Result.error();
@@ -153,13 +192,14 @@ public class SeriesServiceImpl extends ServiceImpl<SeriesDao, Series> implements
     @Transactional(rollbackFor = Exception.class)
     public Result updateSeries(Series series) {
         if (this.updateById(series)) {
-            // 修改演员关系
+            // 修改剧单关系
             if (!series.getCourseIds().isEmpty()) {
                 seriesCourseService.remove(new QueryWrapper<SeriesCourse>().eq("series_id", series.getSeriesId()));
                 for (Long courseId : series.getCourseIds()) {
                     SeriesCourse seriesCourse = new SeriesCourse(series.getSeriesId(), courseId);
                     seriesCourseService.save(seriesCourse);
                 }
+                this.flushSeries(series.getSeriesId());
                 return Result.success();
             }
         }
@@ -171,8 +211,27 @@ public class SeriesServiceImpl extends ServiceImpl<SeriesDao, Series> implements
     public Result deleteSeries(List<Long> idList) {
         if (this.removeByIds(idList)) {
             seriesCourseService.remove(new QueryWrapper<SeriesCourse>().in("series_id", idList));
+            //this.flushSeriesAll();
+            redisUtils.deleteByPattern("page_series_*");
+            idList.forEach(seriesId -> redisUtils.deleteByPattern(String.format("seriesId_*%d", seriesId)));
             return Result.success();
         }
         return Result.error();
+    }
+
+    public void flushSeries() {
+        flushSeries(null);
+    }
+
+    public void flushSeries(Long seriesId) {
+        redisUtils.deleteByPattern("page_series_*");
+        if (seriesId != null) {
+            redisUtils.deleteByPattern(String.format("seriesId_(app_)?%d", seriesId));
+        }
+    }
+
+    public void flushSeriesAll() {
+        redisUtils.deleteByPattern("page_series_*");
+        redisUtils.deleteByPattern("seriesId_*");
     }
 }
